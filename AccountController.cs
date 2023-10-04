571e12491182c63ccc0627330c385bef0a5e51c6
khaihien <146938308+khaihien@users.noreply.github.com>
2023-10-04 20:00:58 +0700

    controller

---
+++ AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private EshopContext _context;
+        public AccountController(EshopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string Username, string Password)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Accounts.ToList());
+        }
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var account = _context.Accounts.Find(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind("Username,Password,Email,Phone,Address,FullName,IsAdmin,Status")] Account account)
+        {
+            if (_context.Accounts.Any(a => a.Username == account.Username))
+            {
+                ViewBag.Error = "Tên đăng nhập đã tồn tại.";
+                return View();
+            }
+            else
+            {
+                _context.Accounts.Add(account);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ ProductTypeController.cs
@@ -0,0 +1,20 @@
+﻿using Eshop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private EshopContext _context;
+        public ProductTypeController(EshopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lstPType = _context.ProductTypes.ToList();
+            return View(lstPType);
+        }
+    }
+}
